db889a3cb4310e80e732e575a1232ad616ab8d81
Ismail <x.iml.7122@gmail.com>
2024-12-25 19:17:16 +0700

    refactor(UserController): simplify sayHello method and integrate UserService

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -12,9 +12,19 @@ import {
   Res,
 } from '@nestjs/common';
 import { Response, Request } from 'express';
+import { UserService } from './user.service';
 
 @Controller('/api/users')
 export class UserController {
+  constructor(private service: UserService) {}
+  
+  @Get('/hello')
+  async sayHello(
+    @Query('name') name: string,
+  ): Promise<string> {
+    return this.service.sayHello(name);
+  }
+
   @Get('/view-hello')
   viewHello(@Query('name') name: string, @Res() response: Response) {
     response.render('index.html', {
@@ -47,13 +57,6 @@ export class UserController {
     return { url: 'https://nestjs.com', statusCode: 301 };
   }
 
-  @Get('/hello')
-  async sayHello(
-    @Query('first_name') firstName: string,
-    @Query('last_name') lastName: string,
-  ): Promise<string> {
-    return `Hello ${firstName} ${lastName}`;
-  }
 
   @Get('/:id')
   getById(@Param('id') id: string): string {
