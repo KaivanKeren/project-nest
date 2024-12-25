bce5690d7bac22f52fa59a7b18e9a0a9bddeb779
Ismail <x.iml.7122@gmail.com>
2024-12-25 21:41:59 +0700

    feat(MemberService): add MemberService for managing connections and email sending

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,6 +4,7 @@ import { UserService } from './user/user.service';
 import { Connection, MongoDBConnection, PostgresConnection } from './connection/connection';
 import { mailService, MailService } from './mail/mail.service';
 import { createUserRepository, UserRepository } from './user-repository/user-repository';
+import { MemberService } from './member/member.service';
 
 @Module({
   controllers: [UserController],
@@ -28,7 +29,8 @@ import { createUserRepository, UserRepository } from './user-repository/user-rep
       provide: UserRepository,
       useFactory: createUserRepository,
       inject: [Connection],
-    }
+    },
+    MemberService
   ],
 })
 export class UserModule {}
